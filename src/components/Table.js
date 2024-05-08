a73b29140b0f613ea420608ef3cac2035aa5850c
Daiane Andrade <daianeandradelt@hotmail.com>
2024-05-07 22:28:54 -0300

    show api data (wip)

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,56 +1,70 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const Table = ({ columns, data }) => {
     return (
-        <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-                <tr>
-                    {columns.map((column, index) => (
-                        <th
-                            key={index}
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            {column}
-                        </th>
-                    ))}
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Ações
-                    </th>
-                </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-                {data.map((row, index) => (
-                    <tr key={index}>
-                        {columns.map((column, colIndex) => (
-                            <td
-                                key={colIndex}
-                                className="px-6 py-4 whitespace-nowrap">
-                                {row[column]}
-                            </td>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500">
+                <thead className="bg-gray-200 text-gray-900 font-bold">
+                    <tr>
+                        {columns.map((column, index) => (
+                            <th
+                                key={index}
+                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                {column}
+                            </th>
                         ))}
-                        <td className="px-6 py-4 whitespace-nowrap">
-                            <button
-                                className={'ml-2'}
-                                onClick={() => console.log('Edit record')}>
-                                <FontAwesomeIcon
-                                    icon={faEdit}
-                                    color="#4b4b4b"
-                                />
-                            </button>
-                            <button
-                                className={'ml-2'}
-                                onClick={() => console.log('Delete record')}>
-                                <FontAwesomeIcon
-                                    icon={faTrash}
-                                    color="#4b4b4b"
-                                />
-                            </button>
-                        </td>
+                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Ações
+                        </th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200 ">
+                    {data.map((row, index) => (
+                        <tr key={index} className="hover:bg-gray-300">
+                            {columns.map((column, colIndex) => (
+                                <td
+                                    key={colIndex}
+                                    className="px-6 py-4 whitespace-nowrap">
+                                    {row[column]}
+                                </td>
+                            ))}
+                            <td className="px-6 py-4 whitespace-nowrap">
+                                <button className={'ml-2'} onClick={() => {}}>
+                                    <FontAwesomeIcon
+                                        icon={faEdit}
+                                        color="#4b4b4b"
+                                        onClick={() => {
+                                            let html = `
+                                            // make a form to edit product
+                                            <input />
+                                            `
+                                            Swal.fire('Editar Produto', html)
+                                        }}
+                                    />
+                                </button>
+                                <button
+                                    className={'ml-2'}
+                                    onClick={() => {
+                                        let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products/${row.Id}`
+                                        axios.delete(url).then(() => {
+                                            window.location.reload()
+                                        })
+                                    }}>
+                                    <FontAwesomeIcon
+                                        icon={faTrash}
+                                        color="#4b4b4b"
+                                    />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,5 +1,6 @@
 import '@/app/global.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import AppLayout from '@/app/(app)/layout'
 import Table from '@/components/Table'
 import MenuActions from '@/components/MenuActions'
@@ -9,11 +10,28 @@ export const metadata = {
 }
 
 const Products = () => {
-    const columns = ['Nome', 'Idade', 'Profissão']
-    const data = [
-        { Nome: 'João', Idade: '30', Profissão: 'Engenheiro' },
-        { Nome: 'Maria', Idade: '25', Profissão: 'Médica' },
-    ]
+    const [products, setProducts] = useState([])
+    let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products`
+    useEffect(() => {
+        axios
+            .get(url)
+            .then(response => {
+                setProducts(response.data.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+
+    const columns = ['Id', 'Nome', 'Preço', 'Categoria', 'Descrição']
+    const data = products.map(product => ({
+        Id: product.id,
+        Nome: product.title,
+        Preço: product.price,
+        Categoria: product.category,
+        Descrição: product.description,
+    }))
+
     return (
         <AppLayout>
             <div className="py-12">
@@ -21,9 +39,12 @@ const Products = () => {
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
                             <div className="flex justify-between items-center">
-                                <h1 className="font-bold text-center" >Lista de Produtos</h1>
+                                <h1 className="font-bold text-center">
+                                    Lista de Produtos
+                                </h1>
                                 <MenuActions />
                             </div>
+                            <br />
                             <Table columns={columns} data={data} />
                         </div>
                     </div>
